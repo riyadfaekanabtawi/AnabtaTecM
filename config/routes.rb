0ed4dfe09e4964dd74f759d851ec224f06823ad3
glori <glori@glori-Anabtawi.(none)>
2013-07-18 02:06:06 +0530

    sda

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ resources :home
   # Sample of regular route:
     match 'gallery/' => 'home#gallery'
       match 'about/' => 'home#about'
-      match 'posts/' => 'posts#about'
-  # Keep in mind you can assign values other than :controller and :action
+      match 'posts/' => 'posts#index'
 
+  # Keep in mind you can assign values other than :controller and :action
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
